9e24915d9e1824b3eccad5b9dd7155d39b141538
Group 4 <pimjitnong@gmail.com>
2020-07-20 10:05:16 -0700

    first commit

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -2,8 +2,11 @@
 // `ng build --prod` replaces `environment.ts` with `environment.prod.ts`.
 // The list of file replacements can be found in `angular.json`.
 
+import { BackendService } from 'src/app/backend.service';
+
 export const environment = {
-  production: false
+  production: false,
+  backendUrl: "http://localhost:3000/",
 };
 
 /*
