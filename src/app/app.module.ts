e3c48c1a847a476b6b8397dececc72921544c86b
Group 4 <chumsri5106@gmail.com>
2020-07-21 11:24:50 -0700

    Version Upload

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,13 +9,15 @@ import { ReactiveFormsModule, FormsModule } from "@angular/forms";
 import { HttpModule } from "@angular/http";
 import { HttpClientModule } from "@angular/common/http";
 import { HomeComponent } from './home/home.component';
-
+import { DragulaModule } from 'ng2-dragula';
+import { CommandAddFileComponent } from './command-add-file/command-add-file.component';
 @NgModule({
   declarations: [
     AppComponent,
     LoginComponent,
     RegisterComponent,
-    HomeComponent
+    HomeComponent,
+    CommandAddFileComponent
   ],
   imports: [
     BrowserModule,
@@ -23,7 +25,8 @@ import { HomeComponent } from './home/home.component';
     ReactiveFormsModule,
     FormsModule,
     HttpModule,
-    HttpClientModule
+    HttpClientModule,
+    DragulaModule.forRoot()
   ],
   providers: [],
   bootstrap: [AppComponent]
