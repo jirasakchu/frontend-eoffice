a19c4ec1c37982ce611b3da05b970be08af6dba0
Group 4 <chumsri5106@gmail.com>
2020-07-22 15:16:06 -0700

    User Notification

--- src/app/about/about.component.ts
+++ src/app/about/about.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { BackendService } from '../backend.service';
+import * as jwt_decode from 'jwt-decode';
 
 @Component({
   selector: 'app-about',
@@ -6,10 +8,17 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./about.component.scss']
 })
 export class AboutComponent implements OnInit {
-
-  constructor() { }
+  userData: any;
+  actionNumber: number = 0;
+  constructor(private backendService: BackendService) { }
 
   ngOnInit(): void {
+    this.userData = jwt_decode(localStorage.getItem("token"));
+    console.log(location.origin);
+
+    this.backendService.getCountDocumentByUser(this.userData.id).then(data => {
+      this.actionNumber = data.dataList.users_action;
+    })
   }
   openNav() {
     //ความกว้างของ slide menu
